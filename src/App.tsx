a644913c2be1cfd39caa7deb619a9abb8fd90a28
Tim Engle <timothy.englew@gmail.com>
2024-01-05 22:27:07 -0800

    ⏰ Setup and implement timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import './App.css';
 import Modal from './components/Modal';
 import Counter from './components/Counter';
 import Visualizer from './components/Visualizer';
 
+enum Directions {
+    Inhale = 'Inhale',
+    HoldBreath = 'Hold\nRelax',
+    Exhale = 'Exhale',
+    Start = 'Press to Start',
+}
+
 function App() {
     const [count, setCount] = useState(0);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [isRunning, setIsRunning] = useState(false);
-    const [direction, setDirection] = useState('Press to start');
+    const [direction, setDirection] = useState(Directions.Start);
+    const [timer, setTimer] = useState(0);
+    const timerFunc = useRef<number | null>(null);
+
+    useEffect(() => {
+        if (timer > 0 && timer < 5) {
+            setDirection(Directions.Inhale);
+        } else if (timer >= 5 && timer < 12) {
+            setDirection(Directions.HoldBreath);
+        } else if (timer >= 12 && timer < 20) {
+            setDirection(Directions.Exhale);
+        } else if (timer >= 20) {
+            setTimer(1);
+            setCount((prevCount) => prevCount + 1);
+        }
+    }, [timer]);
 
     const handleStart = () => {
         if (!isRunning) {
             setIsRunning(true);
+            setTimer(1);
+            setDirection(Directions.Inhale);
+            timerFunc.current = window.setInterval(() => {
+                setTimer((prevTimer) => prevTimer + 1);
+            }, 1000) as unknown as number;
         }
     };
 
     const handleStop = () => {
         if (isRunning) {
             setIsRunning(false);
+            setDirection(Directions.Start);
+            setCount(0);
+            if (timerFunc.current !== null) {
+                clearInterval(timerFunc.current);
+            }
         }
     };
 
